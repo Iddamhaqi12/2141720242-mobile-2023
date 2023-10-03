0817b93a223bb7be475ed3b954598453cc7035be
= <=>
2023-10-03 12:03:31 +0700

    mobile minggu 4

---
+++ week04/src/prak_3.dart
@@ -0,0 +1,17 @@
+void main(){
+  var gifts = {
+    // key: value
+   'first': 'partidge',
+   'second': 'turtledoves',
+   'fith': 1
+  };
+
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 2,
+  };
+
+  print(gifts);
+  print(nobleGases);
+}
