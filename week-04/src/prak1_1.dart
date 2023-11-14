1af51f58639fd8276b32abb2d28ef912fe67bc9a
= <=>
2023-11-14 14:31:55 +0700

    week04

---
+++ week-04/src/prak1_1.dart
@@ -0,0 +1,19 @@
+void main(){
+      List<String?> finalList = List.filled(5, null);
+  finalList[1] = "Iddam Anbiya Haqi"; 
+  finalList[2] = "2141720242";
+
+  print(finalList.length);
+  print(finalList[1]);
+  print(finalList[2]); 
+}
+
+//   var list = [1, 2, 3];
+// assert(list.length == 3);
+// assert(list[1] == 2);
+// print(list.length);
+// print(list[1]);
+
+// list[1] = 1;
+// assert(list[1] == 1);
+// print(list[1]);
